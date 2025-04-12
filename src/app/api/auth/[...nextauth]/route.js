d4b452b1cef85fb22977057ec8403add78eeea5a
Himubey <himubey@hotmail.com>
2025-04-12 18:34:21 +0530

    add postgresql with railway

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -122,6 +122,13 @@ const handler = NextAuth({
   },
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  jwt: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
 });
 
 export { handler as GET, handler as POST }; 
